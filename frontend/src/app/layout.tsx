817925be4c2c31754541a1ae8cf63b9c57e67ef5
xrwong2003 <xrwong2003@gmail.com>
2025-12-09 10:38:42 +0800

    frontend folder

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Montserrat } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const montserrat = Montserrat({
-  variable: "--font-montserrat",
+const poppins = Poppins({
+  variable: "--font-poppins",
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700", "800", "900"],
 });
@@ -23,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${montserrat.variable} font-sans antialiased`}
+        className={`${poppins.variable} font-sans antialiased`}
       >
         {children}
       </body>
